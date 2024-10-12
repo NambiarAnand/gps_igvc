46d7a8c778028ea9ab60666373a70a78b5d95b38
SkywardSpectrum <151830380+SkywardSpectrum@users.noreply.github.com>
2024-10-12 17:12:10 +0530

    first commit

---
+++ ublox_ws/build/ublox/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nambiar/ublox_ws/src/ros2-ublox-zedf9p/ublox
+# Build directory: /home/nambiar/ublox_ws/build/ublox
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ ublox_ws/build/ublox/colcon_command_prefix_build.sh
@@ -0,0 +1,4 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/nambiar/ublox_ws/install/ublox_serialization/share/ublox_serialization/package.sh"
+. "/home/nambiar/ublox_ws/install/ublox_msgs/share/ublox_msgs/package.sh"
+. "/home/nambiar/ublox_ws/install/ublox_gps/share/ublox_gps/package.sh"
